c3c5b4edd14790c49d7c1083eb67dd7a33d81af2
hadesr <56061864+hadesr@users.noreply.github.com>
2020-04-11 20:19:31 +0530

    Update envmt.cpp

--- Assignment 2/Q2/envmt.cpp
+++ Assignment 2/Q2/envmt.cpp
@@ -291,6 +291,11 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, deltaTime);
+
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)

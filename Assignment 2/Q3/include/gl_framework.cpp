0bb9cda615a1030241a19253b6abbe8706ae6e39
hadesr <56061864+hadesr@users.noreply.github.com>
2020-04-11 20:30:53 +0530

    Update gl_framework.cpp

--- Assignment 2/Q3/include/gl_framework.cpp
+++ Assignment 2/Q3/include/gl_framework.cpp
@@ -22,21 +22,6 @@ namespace opengl
   {
     std::cerr<<description<<std::endl;
   }
-  
-  //!GLFW framebuffer resize callback
-  void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-  {
-    //!Resize the viewport to fit the window size - draw to entire window
-    glViewport(0, 0, width, height);
-  }
-  
-  //!GLFW keyboard callback
-  void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-  {
-    //!Close the window if the ESC key was pressed
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-      glfwSetWindowShouldClose(window, GL_TRUE);
-  }
 };  
   
 
